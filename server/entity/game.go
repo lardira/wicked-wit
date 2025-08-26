3c8469fc42eb7116abd156afd41870e08e605615
lardira <danilkefimov@gmail.com>
2025-08-26 15:42:50 +0300

    game and round status model

--- server/entity/game.go
+++ server/entity/game.go
@@ -1,11 +1,20 @@
 package entity
 
+type GameStatus uint
+
+const (
+	GameStatusStarted    = 0
+	GameStatusInProgress = 1
+	GameStatusEnded      = 2
+)
+
 type Game struct {
-	Id           string `json:"id"`
-	Title        string `json:"title"`
-	MaxPlayers   uint   `json:"maxPlayers"`
-	CurrentRound *uint  `json:"currentRound"`
-	MaxRound     uint   `json:"maxRound"`
+	Id           string     `json:"id"`
+	Title        string     `json:"title"`
+	MaxPlayers   uint       `json:"maxPlayers"`
+	CurrentRound *uint      `json:"currentRound"`
+	MaxRound     uint       `json:"maxRound"`
+	Status       GameStatus `json:"status"`
 
 	Timed
 }
